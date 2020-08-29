71147e81a40b6df56f64d8d79902c01fcec5cdb9
karthik-karotra <karthikpatel54@gmail.com>
2020-08-29 15:23:58 +0530

    [Karthik]ADDED: Code For Displaying Board & Choosing Valid Positions

--- TicTacToeProblem/TicTacToeSimulator.js
+++ TicTacToeProblem/TicTacToeSimulator.js
@@ -5,6 +5,11 @@ let board = [];
 const ROWS = 3;
 const COLUMNS = 3;
 let letterAssigned;
+let count = 0;
+const TOTAL_PLAYS = ROWS * COLUMNS;
+var readlineSync = require('readline-sync');
+let row;
+let column;
 let resetBoard = () => {
     for (let i = 0; i < ROWS; i++) {
         board[i] = [];
@@ -13,7 +18,7 @@ let resetBoard = () => {
         }
     }
 };
-let assigningLetterToPlayer = () => {
+let checkingPlayerAndAssignedLetterToHim = () => {
     let random = Math.floor(Math.random() * 10) % 2;
     if (random == 1) {
         letterAssigned = "X";
@@ -26,7 +31,47 @@ let assigningLetterToPlayer = () => {
         console.log('Player to play first is player1 and letter assigned to him is ' + player1);
     }
 };
+let displayTicTacToeBoard = () => {
+    console.log('===============');
+    for (let i = 0; i < ROWS; i++) {
+        for (let j = 0; j < COLUMNS; j++) {
+            process.stdout.write('| ' + board[i][j] + ' |');
+        }
+        console.log();
+        console.log('===============');
+    }
+};
+let changeOneDimentionalPositionToTwoDimentional = (position) => {
+    row = Math.floor((position - 1) / COLUMNS);
+    column = (position - 1) % COLUMNS;
+};
+let setLetter = (row, column, letterAssigned) => {
+    console.log(row);
+    console.log(column);
+    if (board[row][column] == '.') {
+        board[row][column] = letterAssigned;
+        count++;
+    }
+    else {
+        console.log('You cant place there Its already occupied');
+    }
+};
+function playGame() {
+    while (count < TOTAL_PLAYS) {
+        let position = readlineSync.question('Enter the position you want to place your letter (1-9)');
+        // read - p "Enter the position you want to place your letter (1-9)" position
+        if (position > 0 && position <= TOTAL_PLAYS) {
+            changeOneDimentionalPositionToTwoDimentional(position);
+            setLetter(row, column, letterAssigned);
+            displayTicTacToeBoard();
+        }
+        else {
+            console.log('Invalid Position');
+        }
+    }
+}
 resetBoard();
-assigningLetterToPlayer();
-console.log('Letter assigned is ' + letterAssigned);
+checkingPlayerAndAssignedLetterToHim();
+displayTicTacToeBoard();
+playGame();
 //# sourceMappingURL=TicTacToeSimulator.js.map
--- TicTacToeProblem/TicTacToeSimulator.ts
+++ TicTacToeProblem/TicTacToeSimulator.ts
@@ -6,6 +6,11 @@ let board: any[][] = [];
 const ROWS = 3
 const COLUMNS = 3
 let letterAssigned;
+let count = 0;
+const TOTAL_PLAYS = ROWS * COLUMNS;
+var readlineSync = require('readline-sync');
+let row;
+let column;
 
 let resetBoard = () => {
     for (let i: number = 0; i < ROWS; i++) {
@@ -23,12 +28,57 @@ let checkingPlayerAndAssignedLetterToHim = () => {
         let player1 = letterAssigned
         console.log('Player to play first is player1 and letter assigned to him is ' + player1);
     }
-	else {
+    else {
         letterAssigned = "O";
         let player1 = letterAssigned
         console.log('Player to play first is player1 and letter assigned to him is ' + player1);
     }
 }
 
+let displayTicTacToeBoard = () => {
+    console.log('===============');
+    for (let i = 0; i < ROWS; i++) {
+        for (let j = 0; j < COLUMNS; j++) {
+            process.stdout.write('| ' + board[i][j] + ' |');
+        }
+        console.log();
+        console.log('===============');
+    }
+}
+
+let changeOneDimentionalPositionToTwoDimentional = (position) => {
+    row = Math.floor((position - 1) / COLUMNS)
+    column = (position - 1) % COLUMNS
+}
+
+let setLetter = (row, column, letterAssigned) => {
+    console.log(row);
+    console.log(column);
+
+    if (board[row][column] == '.') {
+        board[row][column] = letterAssigned;
+        count++;
+    }
+    else {
+        console.log('You cant place there Its already occupied');
+    }
+}
+
+function playGame() {
+    while (count < TOTAL_PLAYS) {
+        let position = readlineSync.question('Enter the position you want to place your letter (1-9)');
+        if (position > 0 && position <= TOTAL_PLAYS) {
+            changeOneDimentionalPositionToTwoDimentional(position);
+            setLetter(row, column, letterAssigned);
+            displayTicTacToeBoard();
+        }
+        else {
+            console.log('Invalid Position');
+        }
+    }
+}
+
 resetBoard();
 checkingPlayerAndAssignedLetterToHim();
+displayTicTacToeBoard();
+playGame();
